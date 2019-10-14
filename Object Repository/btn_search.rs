361e9c18432a0fd20f9fc4242cacd65c99c20b6c
PonG <PonG@PonG-Win10-Desk>
2019-10-14 10:54:09 +0700

    add Text case add Obj Repository

---
+++ Object Repository/btn_search.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_search</name>
+   <tag></tag>
+   <elementGuidId>582cb05e-0156-4233-997e-fe9d26568adf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='FPdoLc VlcLAe']//input[@name='btnK']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='FPdoLc VlcLAe']//input[@name='btnK']</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/tx_search.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description>//div[@class='vdLsw gsfi']</description>
+   <name>tx_search</name>
+   <tag></tag>
+   <elementGuidId>8954335f-502a-43ea-8b69-777ff8313189</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@class='vdLsw gsfi']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@class='vdLsw gsfi']</value>
+   </webElementProperties>
+</WebElementEntity>
